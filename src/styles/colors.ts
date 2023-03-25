f884a08dee6a391ccb941e4ac606a367b646fc00
Jesus Santana-Roman <jesus.santana0320@gmail.com>
2023-03-25 16:57:19 -0400

    update colors file

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -4,14 +4,20 @@ export const BLACK = "#000000";
 
 // Custom Colors
 export const PRIMARY = "#EC6539"; // a lot of places
-export const GRAY_100 = "#1C1C1C"; // Sound Card Button
-export const GRAY_200 = "#9A9A9A"; // close modal
-export const GRAY_300 = "#9EA0A3"; // Sound off / Sound on
-export const GRAY_400 = "#A7A7A7"; // membership picker sub text
-export const GRAY_500 = "#D7D7D7"; // list item text
-export const GRAY_600 = "#4D4E50"; // list item icon / restore text
-export const GRAY_700 = "#181818"; // Sound Card / Membership Picker
-export const GRAY_800 = "#242424"; // Slider background
-export const GRAY_40 = "rgba(97, 97, 97, 0.4)"; // play button global
-export const BLACK_80 = "rgba(13, 13, 13, 0.8)"; // Lock Background
-export const Charcoal = "#262626"; // border for membership picker
+export const GRAY_100 = "#D7D7D7"; // Sound Card Button
+export const GRAY_200 = "#A7A7A7"; // close modal
+export const GRAY_300 = "#4D4E50"; // Sound off / Sound on
+export const GRAY_400 = "#262626"; // membership picker sub text
+export const GRAY_500 = "#1C1C1C"; // list item text
+export const GRAY_600 = "#181818"; // list item icon / restore text
+export const GRAY_700 = "#0D0D0D"; // Sound Card / Membership Picker
+
+export const PRIMARY_GRADIENT = [
+  "#FF8862",
+  "0.01%",
+  "#EC6539",
+  "0.02%",
+  "#FF9D66",
+  "100",
+];
+export const BLACK_GRADIENT = ["#212121", "0%", "#0D0D0D", "65.57%"];
