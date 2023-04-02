07f85d6775b14b37543707dce3c012c40a03aedb
Jesus Santana-Roman <jesus.santana0320@gmail.com>
2023-04-01 23:40:59 -0400

    progress

--- src/screens/features/index.tsx
+++ src/screens/features/index.tsx
@@ -1,11 +1,134 @@
-import { ScrollView } from "react-native";
-import { SondaPlusText } from "../../components";
+import * as Haptics from "expo-haptics";
+import { Image } from "expo-image";
+import { LinearGradient } from "expo-linear-gradient";
+import {
+  ImageBackground,
+  Pressable,
+  ScrollView,
+  Text,
+  View,
+} from "react-native";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "_store";
+import { Constants } from "_utils";
+import { BaseButton } from "_components";
+import { setMembershipSelected } from "../../store/slices/membershipSelected";
 import styles from "./style";
 
 const Features = () => {
+  const dispatch = useDispatch();
+
+  const { membershipSelected } = useSelector(
+    (state: RootState) => state.membershipSelected
+  );
+
+  const handlePress = (selectedPlan: Constants.planType) => {
+    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+    if (membershipSelected !== selectedPlan) {
+      dispatch(
+        setMembershipSelected(
+          membershipSelected === Constants.planType.Supporter
+            ? Constants.planType.Believer
+            : Constants.planType.Supporter
+        )
+      );
+    }
+  };
   return (
     <ScrollView style={styles.container}>
-      <SondaPlusText />
+      <View style={styles.headerContainer}>
+        <ImageBackground
+          resizeMode="contain"
+          source={
+            membershipSelected === Constants.planType.Supporter
+              ? require("../../assets/images/supporter-card.png")
+              : require("../../assets/images/believer-card.png")
+          }
+          style={styles.backgroundImageContainer}
+          imageStyle={styles.backgroundImage}
+        >
+          <Image
+            source={require("../../assets/images/sonda-plus.png")}
+            style={styles.sondaPlus}
+          />
+          <View style={styles.planButtonContainer}>
+            <Pressable
+              onPress={() => {
+                handlePress(Constants.planType.Supporter);
+              }}
+            >
+              <LinearGradient
+                start={[0, 0]}
+                end={[0, 1]}
+                colors={
+                  membershipSelected === Constants.planType.Supporter
+                    ? ["#EC6539", "#FF8862", "#FF9D66"]
+                    : ["#262626", "#262626"]
+                }
+                style={{ borderRadius: 12 }}
+              >
+                <View style={styles.planButton}>
+                  <Text style={styles.planButtonText}>Supporter</Text>
+                </View>
+              </LinearGradient>
+            </Pressable>
+            <View style={{ width: 8 }}></View>
+            <Pressable
+              onPress={() => {
+                handlePress(Constants.planType.Believer);
+              }}
+            >
+              <LinearGradient
+                start={[0, 0]}
+                end={[0, 1]}
+                colors={
+                  membershipSelected === Constants.planType.Believer
+                    ? ["#EC6539", "#FF8862", "#FF9D66"]
+                    : ["#262626", "#262626"]
+                }
+                style={{ borderRadius: 12 }}
+              >
+                <View style={styles.planButton}>
+                  <Text style={styles.planButtonText}>Believer</Text>
+                </View>
+              </LinearGradient>
+            </Pressable>
+          </View>
+        </ImageBackground>
+      </View>
+      {/* 
+        Image with Text Container 
+        image={require("")
+        text ="All sounds for you to play anytime.
+        Without limits."
+      */}
+      {/* 
+        Image with Text Container 
+        image={require("")
+        text ="Unlock icons to spice up your Home Screen."
+      */}
+      {/* 
+       membershipSelected === Constants.planType.Believer
+        Image with Text Container 
+        image={require("")
+        text ="Unlimited updates. Forever."
+      */}
+      {/* 
+        Image with Text Container 
+        image={require("")
+        text ="No ads, no tracking. Ever."
+      */}
+      <View style={{ height: 20 }}></View>
+      <View>
+        <BaseButton
+          text={
+            membershipSelected === Constants.planType.Supporter
+              ? "$13.00/Year, cancel anytime"
+              : "$45.00 one time. Yours forever."
+          }
+          onPress={() => Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light)}
+        />
+      </View>
     </ScrollView>
   );
 };
--- src/screens/features/style.ts
+++ src/screens/features/style.ts
@@ -3,10 +3,51 @@ import { Colors, Mixins, Spacing, Typography } from "_styles";
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: 100,
+    paddingTop: 32,
     flex: 1,
     backgroundColor: Colors.BLACK,
     paddingHorizontal: Spacing.SCALE_16,
+  },
+  headerContainer: {
+    height: 280,
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  backgroundImageContainer: {
+    alignItems: "center",
+  },
+  backgroundImage: {
+    marginTop: 30,
+    height: 210,
+    width: 270,
+  },
+  sondaPlus: {
+    height: 37,
+    width: 173,
+  },
+  planButtonContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "flex-end",
+    height: 200,
+  },
+  planButton: {
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: Colors.BLACK,
+    height: 50,
+    width: 120,
+    borderRadius: 12,
+    margin: 3,
+  },
+  planButtonText: {
+    fontFamily: Typography.FONT_FAMILY_REGULAR,
+    fontSize: Mixins.scaleFont(16),
+    color: Colors.WHITE,
+    lineHeight: 19,
+    letterSpacing: -2,
   },
 });
 
